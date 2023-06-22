ae5915d92c88063fa43e256a507a8eefd43edce0
josephdattilo03 <jadattilobusiness@gmail.com>
2023-06-22 10:23:51 -0500

    done besides story text

--- src/components/ContactCard/ContactCard.js
+++ src/components/ContactCard/ContactCard.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Component } from "react";
 import "./ContactCard.css"
 import Aos from "aos";
+import {AiFillLinkedin, AiFillGithub} from "react-icons/ai"
 Aos.init()
 
 
@@ -11,6 +12,10 @@ class ContactCard extends Component {
         return(
             <div id="contact-wrap">
                 <h1 id="talk" data-aos="fade-right" data-aos-delay="50">Lets Talk!</h1>
+                <div id="social-bar" data-aos="fade-right" data-aos-delay="50">
+                    <a href="https://www.linkedin.com/in/joseph-dattilo-471436243/"><AiFillLinkedin className="social"/></a>
+                    <a href="https://github.com/josephdattilo03"><AiFillGithub className="social"/></a>
+                </div>
                 <form data-aos="fade-down" data-aos-delay="600">
                     <input id="email-input" type="text" placeholder="Email"></input>
                     <textarea id="email-body" placeholder="Write something..."></textarea>
